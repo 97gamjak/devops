e891c7781b6d71d7f521c1a4f0a69cca0bb6c534
Jakob Gamper <97gamjak@gmail.com>
2025-12-19 18:30:37 +0100

    Fix: update issue link format in logger.py

--- src/devops/logger/logger.py
+++ src/devops/logger/logger.py
@@ -7,7 +7,7 @@ __DEBUG_DEVOPS_CHECKS__ = os.getenv("DEBUG_DEVOPS_CHECKS", "0")
 __DEBUG_DEVOPS_UTILS__ = os.getenv("DEBUG_DEVOPS_UTILS", "0")
 
 # TODO(97gamjak): centralize env logic if needed elsewhere
-# https://github.com/97gamjak/mstd/issues/26
+# https://97gamjak.atlassian.net/browse/DEV-26
 if int(__DEBUG_DEVOPS_CHECKS__) > 0:
     logging.basicConfig(level=logging.DEBUG)
 else:
