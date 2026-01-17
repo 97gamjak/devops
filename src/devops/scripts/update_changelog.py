aea121a95a9b725a83911a931d966cd77df34570
Jakob Gamper <97gamjak@gmail.com>
2026-01-17 22:57:04 +0100

    feat: add optional changelog_path argument to main function in update_changelog script

--- src/devops/scripts/update_changelog.py
+++ src/devops/scripts/update_changelog.py
@@ -1,6 +1,7 @@
 """Script for updating the changelog file."""
 
 import sys
+from pathlib import Path
 
 import typer
 
@@ -13,7 +14,7 @@ app = typer.Typer()
 
 
 @app.command()
-def main(version: str, changelog_path: str | None) -> None:
+def main(version: str, changelog_path: str | None = None) -> None:
     """Update the changelog file with a new version entry.
 
     Parameters
@@ -26,7 +27,8 @@ def main(version: str, changelog_path: str | None) -> None:
     """
     if changelog_path is None:
         changelog_path = config.file.default_changelog_path
-
+    else:
+        changelog_path = Path(changelog_path)
     try:
         update_changelog.update_changelog(version, changelog_path)
         mstd_print(f"✅ CHANGELOG.md updated for version {version}")
